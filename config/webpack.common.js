15d7fa7cdbaa14ade257ab20ade85d9009b2bc97
dongdn195 <dongdn@antsprogrammatic.com>
2021-04-30 16:50:22 +0700

    test

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,75 +2,71 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: path.resolve(__dirname, '..', './src/index.tsx'),
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.scss$/,
-                exclude: /\.module.(scss)$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.module.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            localIdentName: 'ants-[local]-[hash:base64:5]',
-                            camelCase: true,
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/inline',
-            },
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
         ],
-    },
-    output: {
-        path: path.resolve(__dirname, '..', './build'),
-        filename: 'bundle.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', './src/public/index.html'),
-        }),
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /\.module.(scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.module.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: 'ants-[local]-[hash:base64:5]',
+              camelCase: true,
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
     ],
-//     stats: 'errors-only',
- }
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './src/public/index.html'),
+    }),
+  ],
+  //     stats: 'errors-only',
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import {Counter} from './module/components/Counter'
+import { Counter } from './module/components/Counter'
 
 export const App = () => {
-    return (
-        <>
-            <h1>Hello edited hihi Everyone - {process.env.NODE_ENV} - {process.env.name}</h1>
-            <Counter/>
-        </>
-    )
+  const dong = 'trang'
+  return (
+    <>
+          <h1>
+          Hello edited hihi Everyone - {process.env.NODE_ENV} - {process.env.name}
+          </h1>
+
+          <Counter />
+    </>
+  )
 }
