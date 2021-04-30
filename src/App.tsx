0bd13b17b96792c768bfd02562fe2cfce4751f9c
dongdn195 <dongdn@antsprogrammatic.com>
2021-04-30 13:49:42 +0700

    update eslint webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import {Counter} from './module/components/Counter'
+
 export const App = () => {
-    return <h1>Hello</h1>
+    return (
+        <>
+            <h1>Hello edited hihi Everyone - {process.env.NODE_ENV} - {process.env.name}</h1>
+            <Counter/>
+        </>
+    )
 }
