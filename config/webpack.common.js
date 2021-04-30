6e3555f8ce1198a02635761aea52591bda07c3bd
dongdn195 <dongdn@antsprogrammatic.com>
2021-04-30 18:31:53 +0700

    update pkg

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,83 +1,83 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const paths = require('./paths')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const paths = require('./paths');
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    extensions: ['.js', '.json', '.jsx', '.ts', '.tsx'],
-    alias: {
-      Src: paths.appSrc,
-      Assets: `${paths.appSrc}/assets/`,
-      Components: `${paths.appSrc}/components/`,
-      Hooks: `${paths.appSrc}/hooks/`,
-      Modules: `${paths.appSrc}/modules/`,
-      Services: `${paths.appSrc}/services/`,
-      Locale: `${paths.appSrc}/locale/`,
-      react: require.resolve('react'),
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    resolve: {
+        extensions: ['.js', '.json', '.jsx', '.ts', '.tsx'],
+        alias: {
+            Src: paths.appSrc,
+            Assets: `${paths.appSrc}/assets/`,
+            Components: `${paths.appSrc}/components/`,
+            Hooks: `${paths.appSrc}/hooks/`,
+            Modules: `${paths.appSrc}/modules/`,
+            Services: `${paths.appSrc}/services/`,
+            Locale: `${paths.appSrc}/locale/`,
+            react: require.resolve('react'),
+        },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
-        exclude: /\.module.(scss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.module.scss$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: 'ants-[local]-[hash:base64:5]',
-              camelCase: true,
-              sourceMap: true,
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             },
-          },
+            {
+                test: /\.scss$/,
+                exclude: /\.module.(scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.module.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: 'ants-[local]-[hash:base64:5]',
+                            camelCase: true,
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
         ],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
+    },
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/public/index.html'),
+        }),
     ],
-  },
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/public/index.html'),
-    }),
-  ],
-  //     stats: 'errors-only',
-}
+    //     stats: 'errors-only',
+};
